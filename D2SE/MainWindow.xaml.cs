ddfabb04e48e1ecc5e00356cfe0137bead8f4478
DrNoLife <iamdrnolife@gmail.com>
2020-09-26 12:25:49 +0200

    D2SE code

---
+++ D2SE/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace D2SoloEnabler
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Check if rule is active.
+        static string fwRuleName = "Destiny 2 - Solo-Enabler";
+        static string portRangeToBlock = "27000-27200,3097";
+        bool isActive = Soloplay.DoesFWRuleExist(fwRuleName);
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Change values of whatev stuff, dependent on if rule is active or no.
+            statusHandling(isActive);
+        }
+
+        // On click of big boyo!
+        private void soloplayButton_Click(object sender, RoutedEventArgs e)
+        {
+            // If the rule is indeed active. Remove FW rule. Check if still is active. Then do UI changed based on that.
+            if (isActive)
+            {
+                Soloplay.RemoveFirewallRule(fwRuleName);
+                isActive = Soloplay.DoesFWRuleExist(fwRuleName);
+                statusHandling(isActive);
+            }
+
+            // If the rule is not active. We then add the FW rules. Check if they now exist. Then do UI changed based on that.
+            else
+            {
+                // Name. Portrange. Outbound yes/no. UDP yes/no.
+                Soloplay.CreateFWRule(ruleName: fwRuleName, portValue: portRangeToBlock, isOut: true, isUDP: true);
+                Soloplay.CreateFWRule(ruleName: fwRuleName, portValue: portRangeToBlock, isOut: true, isUDP: false);
+                Soloplay.CreateFWRule(ruleName: fwRuleName, portValue: portRangeToBlock, isOut: false, isUDP: true);
+                Soloplay.CreateFWRule(ruleName: fwRuleName, portValue: portRangeToBlock, isOut: false, isUDP: false);
+
+                isActive = true;
+                statusHandling(isActive);
+            }
+        }
+
+        private void statusHandling(bool isActive)
+        {
+            if (isActive)
+            {
+                currentStatus.Text = "Enabled.";
+                soloplayButton.Content = "Disable soloplay";
+            }
+            else
+            {
+                currentStatus.Text = "Disabled.";
+                soloplayButton.Content = "Enable soloplay";
+            }
+        }
+    }
+}
---
+++ D2SE/Soloplay.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetFwTypeLib;
+
+namespace D2SoloEnabler
+{
+    class Soloplay
+    {
+        /// <summary>
+        /// Returns either true or false, dependent on whether or not a rule with the supplied name exists.
+        /// </summary>
+        /// <param name="ruleName">The name of the rule to search for.</param>
+        /// <returns>true if rule exists, otherwise false.</returns>
+        public static bool DoesFWRuleExist(string ruleName)
+        {
+            bool ruleExists = false;
+
+            Type tNetFwPolicy2 = Type.GetTypeFromProgID("HNetCfg.FwPolicy2");
+            INetFwPolicy2 fwPolicy2 = (INetFwPolicy2)Activator.CreateInstance(tNetFwPolicy2);
+
+            foreach (INetFwRule rule in fwPolicy2.Rules)
+            {
+                if (rule.Name == ruleName)
+                {
+                    ruleExists = true;
+                }
+            }
+
+            // If rule exists, return true.. Otherwise false.
+            if (ruleExists)
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Creates a Firewall rule. Use parameters to control name, port, direction and protocol.
+        /// </summary>
+        /// <param name="ruleName">Name of the rule.</param>
+        /// <param name="portValue">Single port: "222". Range: "222-444".</param>
+        /// <param name="isOut">true = outbound rule. False = inbound rule.</param>
+        /// <param name="isUDP">true = UDP. False = TCP.</param>
+        public static void CreateFWRule(string ruleName = "Firewall testing via C#", string portValue = "8000-8005", bool isOut = true, bool isUDP = true)
+        {
+            Type tNetFwPolicy2 = Type.GetTypeFromProgID("HNetCfg.FwPolicy2");
+            INetFwPolicy2 fwPolicy2 = (INetFwPolicy2)Activator.CreateInstance(tNetFwPolicy2);
+            var currentProfiles = fwPolicy2.CurrentProfileTypes;
+
+            // Let's create a new FW rule.
+            INetFwRule2 inboundRule = (INetFwRule2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FWRule"));
+
+            // Block it through firewall
+            inboundRule.Action = NET_FW_ACTION_.NET_FW_ACTION_BLOCK;
+
+            // Quick description for the rule.
+            inboundRule.Description = "Used by the program Solo Enabler. Its use is to enable solo play in Destiny 2.";
+
+            // Set the direction.
+            if (isOut)
+                inboundRule.Direction = NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_OUT;
+            else
+                inboundRule.Direction = NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_IN;
+
+            // Make sure rule is enabled.
+            inboundRule.Enabled = true;
+
+            // Set the name of the FW rule
+            inboundRule.Name = ruleName;
+
+            // Make sure to set the protocol before the ports. TCP = 6. UDP = 17.
+            if (isUDP)
+                inboundRule.Protocol = (int)NetFwTypeLib.NET_FW_IP_PROTOCOL_.NET_FW_IP_PROTOCOL_UDP;
+            else
+                inboundRule.Protocol = (int)NetFwTypeLib.NET_FW_IP_PROTOCOL_.NET_FW_IP_PROTOCOL_TCP;
+
+            // Set the ports.
+            inboundRule.RemotePorts = portValue;
+
+            // Add the rule itself
+            INetFwPolicy2 firewallPolicy = (INetFwPolicy2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+            firewallPolicy.Rules.Add(inboundRule);
+        }
+
+        /// <summary>
+        /// Removes firewall rule, if one with the supplied name exists.
+        /// </summary>
+        /// <param name="ruleName">Name of firewall rule to remove.</param>
+        public static void RemoveFirewallRule(string ruleName)
+        {
+            Type tNetFwPolicy2 = Type.GetTypeFromProgID("HNetCfg.FwPolicy2");
+            INetFwPolicy2 fwPolicy2 = (INetFwPolicy2)Activator.CreateInstance(tNetFwPolicy2);
+
+            // Go through the rule list
+            foreach (INetFwRule rule in fwPolicy2.Rules)
+            {
+                if (rule.Name == ruleName)
+                {
+                    Console.WriteLine("Match! {0}", rule.Name);
+                    fwPolicy2.Rules.Remove(ruleName);               // If a match is found. Then delete the rule with our supplied name.
+                    Console.WriteLine("{0} deleted.", ruleName);
+                }
+            }
+        }
+    }
+}
