9af75edb4bd160550926227ff613fcf3084205cb
Sébastien Giroux <sgiroux@genetec.com>
2020-11-17 20:55:49 -0500

    Issue #5: Added About dialog

--- D2SE/pages/About.xaml.cs
+++ D2SE/pages/About.xaml.cs
@@ -13,16 +13,28 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace D2SoloEnabler.pages
+namespace D2SoloEnabler
 {
     /// <summary>
     /// Interaction logic for About.xaml
     /// </summary>
-    public partial class About : Page
+    public partial class About
     {
         public About()
         {
             InitializeComponent();
+        }
+
+        public event EventHandler Closed;
+
+        private void OnCloseButtonClicked(object sender, RoutedEventArgs e)
+        {
+            Closed?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void OnButtonRepoClicked(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/DrNoLife/Destiny-2-Solo-Enabler");
         }
     }
 }
